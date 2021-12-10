d14b10b8ef2316541052e95cb5ae89301d9c940e
Akshay Sankhla <akshaysankhla01@gmail.com>
2021-12-10 21:12:52 +0530

    Minor changes in comments

--- door delight/fav/src/main/java/com/stackroute/favoriteservice/service/RestaurantServiceImpl.java
+++ door delight/fav/src/main/java/com/stackroute/favoriteservice/service/RestaurantServiceImpl.java
@@ -26,7 +26,7 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     /***
      * providing business logic for addToFavourites request handler in FavouriteController class.
-     * @param favourite - Favorite type object received containing emailid and News to be added to favorite list of
+     * @param favourite - Favorite type object received containing emailid and Restaurant to be added to favorite list of
      *                 user with email provided by attribute 'emailid'
      * @return - returns favorite list of user with emailid received from request handler in case of success.
      * @throws FavouriteAlreadyExistsException - exception thrown in case if the restaurant object received from request
@@ -55,10 +55,10 @@ public class RestaurantServiceImpl implements RestaurantService {
     /***
      * providing business logic for removeFromFavourites request handler in FavoriteController class.
      * @param emailid - type String variable for finding specific user's favorite list.
-     * @param id - type String variable for finding specific news from user's favorite list.
-     * @return - returns favorite list of user with email in case of success.
+     * @param id - type String variable for finding specific restaurant from user's favorite list.
+     * @return - returns favorite restaurant list of user with email in case of success.
      * @throws RuntimeException - throws FavouriteListDoesNotExistsException in case the requested favourite list
-     *          does not exists and FavouriteDoesNotExistsException in case the requested news does not exists in
+     *          does not exists and FavouriteDoesNotExistsException in case the requested restaurant does not exists in
      *          user's favorite list.
      */
     @Override
